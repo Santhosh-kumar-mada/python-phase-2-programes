026d694b06effe54b99fa6c5bfbe1dfa583d7c8e
Santhosh-kumar-mada <259y5a3905@ksrmce.ac.in>
2026-02-26 09:48:27 +0530

    hi

---
+++ pg008-trees.py
@@ -0,0 +1,11 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+n1 = Node(1)
+n2 = Node(2)
+n3 = Node(3)
+n4 = Node(4)
+n5 = Node(5)
